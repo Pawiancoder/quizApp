553b9a243f7bcd70f72a729889aa94cec0681dd9
Pawiancoder <maksiuoechsler@outlook.com>
2024-02-07 18:07:23 +0100

    create Files

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+let questions = [
+    {
+        question: "Was ist die Hauptstadt von Deutschland?",
+        answers: ["Berlin", "Hamburg", "Rom", "Wien"],
+        correctAnswer: 0
+    },
+    {
+        question: "Was ist 2+2?",
+        answers: [5, 1, 2, 4],
+        correctAnswer: 2
+    }
+]
