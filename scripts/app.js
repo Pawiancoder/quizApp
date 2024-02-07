99b5acfb807f85b23f1d47c22c08705c3bd598e3
Pawiancoder <maksiuoechsler@outlook.com>
2024-02-07 19:08:05 +0100

    Added Questions Part1

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,44 @@
 let questions = [
-    {
-        question: "Was ist die Hauptstadt von Deutschland?",
-        answers: ["Berlin", "Hamburg", "Rom", "Wien"],
-        correctAnswer: 0
+    {//1
+        question: "Wie viel Wasser pro Minute in Litern geht durch ein C-Strahlrohr mit Mundstück?",
+        answers: [50, 100, 200, 350],
+        correctAnswer: [1]
     },
-    {
-        question: "Was ist 2+2?",
-        answers: [5, 1, 2, 4],
-        correctAnswer: 2
-    }
+    {//2
+        question: "Was bedeutet LF16/12",
+        answers: ["Pumpenleistung 1.600 Liter / min", "Pumpenleistung 1.200 Liter pro Minute", "Wassertank mit 1.600 Liter Inhalt", "Wassertank mit 1.200 Litern Inhalt"],
+        correctAnswer: [0, 3]
+    },
+    {//3
+        question: "Was bedeutet die 150 auf einem Hydrantenschild?",
+        answers: ["Durchflussmenge 150 Liter / Minute", "150 bar Wasserdruck", "Leitungsdurchmesser von 150mm", "Leitungsdurchmesser von 150cm"],
+        correctAnswer: [2]
+    },
+    {//4
+        question: "Welche Frage gehört nicht zu dem 5 Ws?",
+        answers: ["Was ist passiert?", "Wo ist es passiert?", "Wer ruft an?", "Wie viele Hydranten sind vor Ort?"],
+        correctAnswer: [3]
+    },
+    {//5
+        question: "Was sind die Funkrufnamen der Feuerwehr?",
+        answers: ["Florian & Florentine", "Harrald und Harraldine", "Flämmchen und Flamme", "Glut und Feuer"],
+        correctAnswer: [0]
+    },
+    {//6
+        question: "Wer setzt beim Löschangriff (Löschgruppe) den Verteiler?",
+        answers: ["Gruppenführer", "Angriffstrupp", "Melder", "Wassertrupp"],
+        correctAnswer: [1]
+    },
+    {//7
+        question: "Welchen Knoten gibt es nicht (leider)?",
+        answers: ["Zimmermannsschlag", "Mastwurf", "Achterknoten", "Bienenstich"],
+        correctAnswer: [3]
+    },
+    {//8
+        question: "Wer setzt beim Löschangriff (Löschgruppe) den Verteiler?",
+        answers: ["Gruppenführer", "Angriffstrupp", "Melder", "Wassertrupp"],
+        correctAnswer: [1]
+    },
 ]
 
 function showResultField() {
